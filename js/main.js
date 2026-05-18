373a00efd198995f874e205690de2b93da505120
lya8544-spe <lya852944@gmail.com>
2026-05-18 22:30:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -96,3 +96,52 @@ window.addEventListener("load", () => {
   }
 
 });
+
+
+
+const publicCakesContainer =
+  document.getElementById("publicCakesContainer");
+
+function loadPublicCakes() {
+
+  db.collection("cakes")
+    .orderBy("createdAt", "desc")
+    .onSnapshot(snapshot => {
+
+      publicCakesContainer.innerHTML = "";
+
+      snapshot.forEach(doc => {
+
+        const cake = doc.data();
+        const id = doc.id;
+
+        const card = document.createElement("div");
+        card.classList.add("cake-card");
+
+        card.innerHTML = `
+          <img src="${cake.image}" />
+          <h3>${cake.name}</h3>
+          <p>${cake.description}</p>
+          <p>💰 ${cake.price}</p>
+          <p>📂 ${cake.category}</p>
+
+          <button class="order-btn">طلب الكيكة</button>
+        `;
+
+        card.querySelector(".order-btn")
+          .addEventListener("click", () => {
+
+            // تحويل المستخدم لصفحة الطلب مع ID الكيكة
+            window.location.href = `order1.html?cakeId=${id}`;
+
+          });
+
+        publicCakesContainer.appendChild(card);
+
+      });
+
+    });
+
+}
+
+loadPublicCakes();
